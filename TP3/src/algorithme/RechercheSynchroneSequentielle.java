552ab6493a1b909e9324444f7d7e3c7455b2e59d
Vo Lam Nhat Khuong <volamnhatkhuong@gmail.com>
2021-01-24 16:55:06 +0100

    sync seq

---
+++ TP3/src/algorithme/RechercheSynchroneSequentielle.java
@@ -0,0 +1,33 @@
+package algorithme;
+
+import java.util.List;
+import java.util.Optional;
+
+import javax.ws.rs.client.Client;
+
+import infrastructure.jaxrs.HyperLien;
+import modele.AlgorithmeRecherche;
+import modele.Bibliotheque;
+import modele.ImplemNomAlgorithme;
+import modele.Livre;
+import modele.NomAlgorithme;
+import modele.RechercheSynchroneAbstraite;
+
+public class RechercheSynchroneSequentielle extends RechercheSynchroneAbstraite implements AlgorithmeRecherche {
+
+	@Override
+	public Optional<HyperLien<Livre>> chercher(Livre l, List<HyperLien<Bibliotheque>> bibliotheques, Client client) {
+		Optional<HyperLien<Livre>> buffer;
+		for(HyperLien<Bibliotheque> h: bibliotheques) {
+			buffer = rechercheSync(h, l, client);
+			if (buffer.isPresent()) return buffer;
+		}
+		return Optional.empty();
+	}
+
+	@Override
+	public NomAlgorithme nom() {
+		return new ImplemNomAlgorithme("recherche sync seq");
+	}
+
+}
